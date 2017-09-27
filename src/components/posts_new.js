83d3fe9914e33b1a5251d48dbe0c990f278cc720
nicolasmsg <nicolas.msg@protonmail.com>
2017-09-27 16:19:57 +0700

    added PostsNew form using redux-form, implemented form validation, but the submit button is not implemented yet

---
+++ src/components/posts_new.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+
+  renderField(field){
+    const { meta: { touched, error } } = field;
+    const mclassName = `form-group ${touched && error ? "has-danger" : ""}`;
+
+    return (
+      <div className={mclassName}>
+        <label> {field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} 
+        />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+    </div>
+    )
+  }
+
+  onSubmit(values){
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+          <Field
+          label=" Title"
+          name="title"
+          component={this.renderField}
+         /> 
+         <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+         /> 
+         <Field
+          label="Post content"
+          name="content"
+          component={this.renderField}
+         /> 
+         <button type="submit" className="btn btn-primary">
+          Save
+         </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values){
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
